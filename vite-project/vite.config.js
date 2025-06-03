684385ad8829585386ba5741c392cdd8c1874e00
THalsell <thalsell@banyanlabs.io>
2025-06-03 16:28:23 -0500

    Prepare for GitHub Pages deployment

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  base: '/tiffany-portfolio/', // required for GitHub Pages
+  base: '/tiffany-portfolio/',
   plugins: [react()],
-});
+})
